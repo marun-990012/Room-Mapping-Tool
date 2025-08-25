9cddec62c033d101c45f83f6242b6ed2a28da3c3
marun-990012 <marun990012@gmail.com>
2025-08-25 19:20:36 +0530

    updated

--- frontend/src/pages/Navbar.jsx
+++ frontend/src/pages/Navbar.jsx
@@ -8,8 +8,13 @@ function Navbar(){
 };
 
 return(
-    <div className="z-[9999] bg-white shadow rounded-b-sm text-black fixed top-0 left-0 w-full h-10 flex items-center justify-between px-4 shadow z-50 flex justify-end">
-        {localStorage.getItem('token')?<button className="bg-gray-200 hover:bg-gray-300 px-3 rounded cursor-pointer transition-all duration-300" onClick={handleLogout}>Logout</button>:<button onClick={()=>navigate('/signin')} className="bg-gray-200 hover:bg-gray-300 px-3 rounded cursor-pointer transition-all duration-300">Login</button>}
+    <div className="z-[9999] bg-white shadow rounded-b-sm text-black fixed top-0 left-0 w-full h-10 flex items-center justify-between px-4 shadow z-50">
+        <div>
+            <p>Room Mapping</p>
+        </div>
+        <div>
+            {localStorage.getItem('token')?<button className="bg-gray-200 hover:bg-gray-300 px-3 rounded cursor-pointer transition-all duration-300" onClick={handleLogout}>Logout</button>:<button onClick={()=>navigate('/signin')} className="bg-gray-200 hover:bg-gray-300 px-3 rounded cursor-pointer transition-all duration-300">Login</button>}
+        </div>
     </div>
 )
 }
