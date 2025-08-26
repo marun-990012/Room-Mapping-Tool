4c82ad188beb8a6304cc415c25cf71ba672a1165
marun-990012 <marun990012@gmail.com>
2025-08-26 12:25:07 +0530

    updated

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -77,7 +77,7 @@ export default function Register() {
               <input
                 type="text"
                 name="name"
-                placeholder="John Doe"
+                placeholder="xyz"
                 value={formData.name}
                 onChange={handleChange}
                 className="w-full border border-gray-300 rounded-lg pl-10 pr-3 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
