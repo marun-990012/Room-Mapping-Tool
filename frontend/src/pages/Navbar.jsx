9170206d70d2e99b2e5ff2ec0806b4ad1e860e7d
marun-990012 <marun990012@gmail.com>
2025-08-26 12:13:41 +0530

    updated

--- frontend/src/pages/Navbar.jsx
+++ frontend/src/pages/Navbar.jsx
@@ -1,7 +1,8 @@
 import { useNavigate } from "react-router-dom";
 
 function Navbar(){
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  
   const handleLogout = () => {
   localStorage.removeItem("token");
   navigate("/signin");
